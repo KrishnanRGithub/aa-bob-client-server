37b4be715bf43211ee2f10067e42eb1ccba46998
KrishnanRGithub <vinokrish001@gmail.com>
2023-01-05 10:35:17 +0530

    fixed updating consent (kinda)

--- client/react-native/src/screens/tab/Profile.js
+++ client/react-native/src/screens/tab/Profile.js
@@ -4,9 +4,30 @@ import AppHeader from "../../components/AppHeader";
 import Header from "../../components/Header";
 import ListButton from "../../components/ListButton";
 import RefreshScreen from "../../components/RefreshScreen";
-import { signoutSession } from "../../helpers/sessionHandler";
-export default function Profile({ navigation }) {
+import { signoutSession, storeSession } from "../../helpers/sessionHandler";
+import { getSession } from "../../helpers/sessionHandler";
 
+export default function Profile({ navigation }) {
+  const [userDetails,setUserDetails] = useState(null);
+
+
+  useEffect(() => {
+    getSession("user").then((val) => {
+      setUserDetails(val);
+    });
+  }, []); // empty array ensures that the effect only runs once
+
+  async function removeAAUserIdFromSession(){
+    try{
+      userDetails["trackingId"] = null;
+      userDetails["referenceId"]=null;
+      await storeSession("user",userDetails);
+      console.log(await getSession("user"));
+    }
+    catch(err){
+      console.log(err);
+    } 
+}
   return (
     <RefreshScreen  onRefresh={()=>{console.log("Refreshing in Profile")}}>
       <AppBackground>
@@ -15,6 +36,10 @@ export default function Profile({ navigation }) {
           <ListButton 
               item={{text:"Logout",icon:"logout"}} 
               onPress={()=>{signoutSession(); navigation.navigate("Login");}} 
+            /> 
+          <ListButton 
+              item={{text:"Alow access in Account Aggregator",icon:"sync"}} 
+              onPress={async ()=>{await removeAAUserIdFromSession();  navigation.navigate("StartScreen"); }} 
             />    
       </AppBackground>
     </RefreshScreen>
